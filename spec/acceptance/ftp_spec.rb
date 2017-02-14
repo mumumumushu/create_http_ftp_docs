d6c834e30b0c866bd868199bfc4a30825ef7f423
mumumumushu <mumumumushu@gmail.com>
2017-02-14 14:40:25 +0800

    fix syntax error in ftp_spec

--- spec/acceptance/ftp_spec.rb
+++ spec/acceptance/ftp_spec.rb
@@ -6,91 +6,89 @@ resource "数据源 FTP" do
 
   Object.const_set("FtpController", ApplicationController)  
 
-	Settings.http.each do |item|
+	Settings.ftp.each do |item|
 		_info = item[1]
 	
 		"FtpController".constantize.class_eval do 
 			# 获得body数据
 			define_method "#{item[0].downcase.tr_s("::", "_")}" do 
 				# 使用def 会在 对应 ApplicationController 中寻找 _info
-					def initialize
-						_info = item[0]
-				    _info.each do |k, v|
-				      instance_variable_set "@#{k}", v
-				    end
-				    @process_file_infos = []
-				    # @process_result_info = { :start_time => Time.now.to_f }
-				  end
+				today = Time.zone.now.to_date
+		    today_string = to_date_string today # today.strftime('%Y%m%d')
+		    day_to_fetch = @day_to_fetch || 1
+		    last_day_string = to_date_string(today - day_to_fetch)
 
-					def get_report_time_string filename
-				    report_time_string = filename.send(_info["get_report_time_string"])
-				  end
-
-				  def ftpfile_format day
-				    # _info["filename"].tr_s("#\{}", to_date_string(day))
-				  end
-
-					def connect!
-					  @connection = Net::FTP.new
-					  @connection.connect(@server, @port)
-					  @connection.passive = @passive || false
-					  @connection.login(@user, @password)
-					end
-
-					def close!
-					  @connection.close if @connection
-					  @connection = nil
-					end
-
-					def to_date_string datetime
-				    date_string = datetime.strftime('%Y%m%d')
-				  end
-
-				  def to_datetime_string report_time
-				    report_time.strftime('%Y%m%d%H')
-				  end
-
-				  def process
-						
-						today = Time.zone.now.to_date
-				    today_string = to_date_string today # today.strftime('%Y%m%d')
-				    day_to_fetch = @day_to_fetch || 1
-				    last_day_string = to_date_string(today - day_to_fetch)
-
-				    # @last_report_time = Time.zone.parse(time_string) 
-				    p "+===========  start connect  ==============="
-				    connect! unless @connection
-				    p "+===========  connect success  ==============="
-				    @connection.chdir @remote_dir
-				    
-				    file_arr = []
-				    file_infos = []
-				    (0..day_to_fetch).each do |index|
-				      file_arr.concat @connection.nlst(ftpfile_format(today-index)) rescue []
-				    end
-				    file_arr.each do |filename|
-				      report_time_string = get_report_time_string filename
-				      filename = filename.encode! 'utf-8', 'gb2312', {:invalid => :replace}
-				      file_infos << [report_time_string, filename]
-				      #  文件名的时间, 转码后文件名
-				    end
-				    p "+===========  file_info   ==============="
-				    p file_infos
-				    file_infos = file_infos.sort_by { |k, v| k }
-				    # @is_process = false
-				    p "+===========  file_info after sort_by  ==============="
-				    p file_infos
-				    close!
-					end
-
-
-
-
+		    # @last_report_time = Time.zone.parse(time_string) 
+		    p "+===========  start connect  ==============="
+		    connect! unless @connection
+		    p "+===========  connect success  ==============="
+		    @connection.chdir @remote_dir
+		    
+		    file_arr = []
+		    file_infos = []
+		    (0..day_to_fetch).each do |index|
+		      file_arr.concat @connection.nlst(ftpfile_format(today-index)) rescue []
+		    end
+		    file_arr.each do |filename|
+		      report_time_string = get_report_time_string filename
+		      filename = filename.encode! 'utf-8', 'gb2312', {:invalid => :replace}
+		      file_infos << [report_time_string, filename]
+		      #  文件名的时间, 转码后文件名
+		    end
+		    p "+===========  file_info   ==============="
+		    p file_infos
+		    file_infos = file_infos.sort_by { |k, v| k }
+		    # @is_process = false
+		    p "+===========  file_info after sort_by  ==============="
+		    p file_infos
+		    close!
+		    render :json => {aa: file_infos}
 		 	end
+
+		 	define_method "initialize" do 
+		    _info.each do |k, v|
+		      instance_variable_set "@#{k}", v
+		    end
+		    @process_file_infos = []
+		    # @process_result_info = { :start_time => Time.now.to_f }
+		  end
+
+			define_method "get_report_time_string" do |filename|
+		    report_time_string = filename.send(_info["get_report_time_string"])
+		  end
+
+		  define_method "ftpfile_format" do |day|
+		    # _info["filename"].tr_s("#\{}", to_date_string(day))
+		  end
+
+			define_method "connect!" do
+			  @connection = Net::FTP.new
+			  @connection.connect(@server, @port)
+			  @connection.passive = @passive || false
+			  @connection.login(@user, @password)
+			end
+
+			define_method "close!" do
+			  @connection.close if @connection
+			  @connection = nil
+			end
+
+			define_method "to_date_string" do |datetime|
+		    date_string = datetime.strftime('%Y%m%d')
+		  end
+
+		  define_method "to_datetime_string" do |report_time|
+		    report_time.strftime('%Y%m%d%H')
+		  end
+
+		 #  define_method "process" do
+				
+				
+			# end
 	  end
 
 		# 生成文档
-		get _info["url"] do
+		get item[0].downcase.tr_s("::", "_") do
 			
 			# parameter "周期", "#{_info["describe"]["period"]}"
 			# parameter "工程", "#{_info["describe"]["project"]}"
